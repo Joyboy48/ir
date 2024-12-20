0cd981d8e53c95e5dd234642f7c4c900dcc7ff66
Joyboy48 <astitvaarya9589@gmail.com>
2024-12-20 23:47:21 +0530

    commit

--- ir2/app/build.gradle.kts
+++ ir2/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.adapters_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.adapters_app"
--- ir2/app/src/main/java/com/example/adapters_app/MainActivity.kt
+++ ir2/app/src/main/java/com/example/adapters_app/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.adapters_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //1 Initialising the listView
+        val listView = findViewById<ListView>(R.id.listView)
+
+        //2 Data source
+         val list = arrayOf(
+             "A","B","C","D","E","A",
+             "B","C","D","E","A","B",
+             "C","D","E","A","B","C",
+             "D","E","A","B","C","D",
+             "E","A","B","C","D","E"
+         )
+
+        //3 Adapter
+        val myAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            list
+        )
+
+        //4 Set adaptor to list view
+        listView.adapter = myAdapter
+
+
     }
 }
