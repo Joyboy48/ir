f1111262acbb502fabe2626d0f522bb869b086c5
Joyboy48 <astitvaarya9589@gmail.com>
2024-12-25 23:26:31 +0530

    commit

---
+++ grocery_app/app/src/main/java/com/example/groceryapp/itemModel.kt
@@ -0,0 +1,3 @@
+package com.example.groceryapp
+
+data class itemModel(val name:String,val description:String,val image:Int)
---
+++ grocery_app/app/src/main/java/com/example/groceryapp/myAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.groceryapp
+
+import androidx.recyclerview.widget.RecyclerView
+
+class myAdapter(val itemList: ArrayList<itemModel>):RecyclerView.Adapter<> {
+}
