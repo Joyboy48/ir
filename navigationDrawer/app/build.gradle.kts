6e67ae23d51957d47ccec7074ff83c2d423df98e
Joyboy48 <astitvaarya9589@gmail.com>
2025-01-03 18:58:49 +0530

    implemented

--- navigationDrawer/app/build.gradle.kts
+++ navigationDrawer/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.navigationdrawer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.navigationdrawer"
--- navigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ navigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.example.navigationdrawer
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout:DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        drawerLayout =findViewById(R.id.drawerLayout)
+        toggle =  ActionBarDrawerToggle(
+             this@MainActivity,
+             drawerLayout,
+            R.string.open,
+            R.string.close
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //functionality for the nav drawer items
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setNavigationItemSelectedListener {
+            it.isChecked = true
+            when(it.itemId){
+                R.id.home -> {
+                    replaceFragment(HomeFragment(),it.title.toString())
+                }
+                R.id.message -> {
+                    replaceFragment(MessageFragment(),it.title.toString())
+                }
+                R.id.setting -> {
+                    replaceFragment(SettingFragment(),it.title.toString())
+                }
+                R.id.login -> {
+                    replaceFragment(LoginFragment(),it.title.toString())
+                }
+            }
+
+            true
         }
+    }
+
+    private fun replaceFragment(fragment:Fragment,title: String){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout1, fragment)
+        fragmentTransaction.commit()
+
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
